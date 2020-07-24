897ded16272ff9be60b9d679800a39dd153e9d78
Masaiha <masanori.iha@gmail.com>
2020-07-24 13:09:39 -0300

    criancao mappings de entidates

---
+++ Loja/src/MASAIO.Data/Mappings/ProdutoMappings.cs
@@ -0,0 +1,32 @@
+﻿using MASAIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MASAIO.Data.Mappings
+{
+    public class ProdutoMappings : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(150)");
+
+            builder.Property(p => p.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.Property(p => p.Valor)
+                .IsRequired();
+
+            builder.Property(p => p.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
